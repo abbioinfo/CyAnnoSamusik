558a158b7fc016d1e83df93567ccd431d305b3f0
abhinav kaushik <55936153+abbioinfo@users.noreply.github.com>
2020-09-16 20:59:00 -0700

    Update functions.py

--- src/functions.py
+++ src/functions.py
@@ -1538,15 +1538,15 @@ def CelltypePredPerSample(expr,SelfObject,relevantMarkers,indx2ct,origLabels,Unk
         #recall = recall_score(origLabels2,predictedLabels2, average='micro')
         #result2 = printACC(indx2ct, origLabels2, predictedLabels2, sampleID, F1, precision, recall, "Without_UnGated")
         #result = pa.concat([result,result2], axis=0)
-        annotatedExpression = pa.concat([expr, pa.DataFrame(predictedLabels, columns=["labels"], index=expr.index), pa.DataFrame(origLabels.values, columns=["Truelabels"], index=expr.index)], axis=1)
+        annotatedExpression = pa.concat([expr, pa.DataFrame(predictedLabels, columns=["Predictedlabels"], index=expr.index)], axis=1)
         len(predictedLabels)
-        name = str(ProjectName) + "/" + str(sampleID) + '_labled_expr.csv'
+        name = str(ProjectName) + "/" + str(sampleID) + '_labelled_expr.csv'
         print("Writing file.." + name)
         annotatedExpression.to_csv(name)  ## write best cell type predicted for this sample
     else:
         print ("Merging...")
         annotatedExpression = pa.concat([expr, pa.DataFrame(predictedLabels, columns=["labels"], index=expr.index)], axis=1)
-        name = str(ProjectName) + "/" + str(sampleID) + '_labled_expr.csv'
+        name = str(ProjectName) + "/" + str(sampleID) + '_labelled_expr.csv'
         print("Writing file.." + name)
         annotatedExpression.to_csv(name)  ## write best cell type predicted for this sample 
     f.close()
